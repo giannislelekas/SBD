48f29668c14052b90e02a60befe92c75c22af32d
Michail Vrachasotakis <mikvrax@gmail.com>
2018-09-21 09:20:01 +0200

    Improved RDDs implementation

--- RDDs/src/main/scala/RDDs.scala
+++ RDDs/src/main/scala/RDDs.scala
@@ -11,21 +11,17 @@ object implementationRDD {
     val sc = new SparkContext(conf)
     
     // parsing the csv files into input variable 
-    val input = sc.textFile("/home/giannislelekas/Desktop/TU/Supercomputing for Big Data - ET4310/Labs/Lab1/SBD-2018/data/segment/*.csv")
+    val input = sc.textFile("../*.csv")
     printf("Count %d\n", input.count)
     
     //for each field extract the corresponding fields with a map(split), using a tab "\t" as a delimiter and considering blanks with "-1".
     val fields = input.map(x => x.split("\t", -1))
-    //fields.filter(x=>x(1).contains("20180218"))
-    val names = fields.map(x=>x(23)).filter(x=>x != "")
-    
+    val tuples = fields.filter(x=>x(23) != "").map(x=>(x(1).substring(0,8),x(23)))
     
     // firstly discard the digits pointing to the reference of the name in the article; then separate, filter out blanks and FP and mapReduce. Finally sort and keep 10 highest
-    names.map(x=>x.replaceAll("[0-9]","")).flatMap(x=>x.split("[;,]")).filter(x=>x!="" && x!="ParentCategory" && x!="CategoryType" && x!="Type ParentCategory").map(x=>(x,1)).reduceByKey(_+_).map(_.swap).sortByKey(false).map(_.swap).take(10)
+    val topics = tuples.map(x => (x._1,x._2.replaceAll("[0-9]",""))).flatMapValues(x=> x.split("[;,]")).filter(x=>x._2!="" && x._2!="ParentCategory" && x._2!="CategoryType" && x._2!="Type ParentCategory").map(x=>((x._1,x._2),1)).reduceByKey(_+_).map(_.swap).sortByKey(false).map(_.swap).map(x=> (x._1._1,(x._1._2,x._2))).groupByKey().map(x=>Array(x._1,x._2.slice(0,10)))
+    topics.foreach(a => println(a.mkString(" ")))
     
-    //names.flatMap(x=>x.split(";")).map(x=>x.split(",")(0)).map(x=>(x,1)).reduceByKey(_+_).map(_.swap).sortByKey(false).take(10).foreach(println)
-    
-    //println("Hello")
     sc.stop()
   }
 }
