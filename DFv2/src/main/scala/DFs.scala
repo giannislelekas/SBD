b1b3583af02604c5b236d6a82d974d97030bb7ea
Michail Vrachasotakis <mikvrax@gmail.com>
2018-09-22 11:10:38 +0200

    Fixed bug when reading some files and added more extensive comments

--- DFv2/src/main/scala/DFs.scala
+++ DFv2/src/main/scala/DFs.scala
@@ -10,9 +10,10 @@ import org.apache.log4j.{Level, Logger}
 object dataDFs{
     
     def main(args: Array[String]) {
+      // create the schema of the GDelt GKG data
         val schema = 
             StructType(
-                Array(
+               Array(
                     StructField("GKGRECORDID", StringType, nullable=true),
                     StructField("DATE", TimestampType, nullable=true),
                     StructField("SourceCollectionIdentifier", IntegerType, nullable=true),
@@ -43,40 +44,51 @@ object dataDFs{
                 )
             )
             
+        // create a SparakSession needed for using the DataFrames and DataSets APIs
         val spark = SparkSession
             .builder
             .appName("Lab 1 - DFs")
             .config("spark.master", "local")
             .getOrCreate()
+        
         import spark.implicits._
         
-        //val sc = spark.sparkContext
-        
-        
+        // Read the input data, separate the fields and store them in a DataSet
         val ds = spark.read
-                        .schema(schema)
-                        .option("sep", "\t")
-                        .option("timestampFormat", "yyyyMMddHHmmss")
-                        .csv("/home/giannislelekas/Desktop/TU/Supercomputing for Big Data - ET4310/Labs/Lab1/SBD-2018/data/segment/*.csv")
-                    
+            .schema(schema)
+            .option("sep", "\t")
+            .option("timestampFormat", "yyyyMMddHHmmss")
+            .csv("../*.csv")            
         
-        // Map to the AllNames field for the references within each news feed. Filter out blanks
-        val df = ds.map(x => (x.getAs[Timestamp]("DATE").toString().substring(0,10),        x.getAs[String]("AllNames")))
-        .filter(x => x._2 != null)
-        .withColumn("_2", regexp_replace(col("_2"), "[0-9]", ""))
-        .map(x => (x.getAs[String]("_1"), x.getAs[String]("_2").split("[;,]")))
-        .withColumn("_2", explode($"_2"))
-        .filter(x => x.getAs[String]("_2")!= "" && x.getAs[String]("_2")!="Type ParentCategory").map(x => (x.getAs[String]("_1"), x.getAs[String]("_2"), 1))
-        .groupBy("_1", "_2").agg(sum("_3")).sort(desc("sum(_3)"))
+        val df = ds
+            // filter out rows with empty AllNames columns
+            .filter(x => x.getAs[String]("AllNames") != null)
+            // create tuples of the (Date, AllNames) form
+            .map(x => (x.getAs[Timestamp]("DATE").toString().substring(0,10), x.getAs[String]("AllNames")))
+            // remove the offsets using a regular expression that captures only digits
+            .withColumn("_2", regexp_replace(col("_2"), "[0-9]", ""))
+            // separate Names
+            .map(x => (x.getAs[String]("_1"), x.getAs[String]("_2").split("[;,]")))
+            // flatten each AllNames to a single record per Name
+            .withColumn("_2", explode($"_2"))
+            // remove wrong values
+            .filter(x => x.getAs[String]("_2")!= "" && x.getAs[String]("_2")!="Type ParentCategory")
+            // emit 1 for each (Date, Name) pair
+            .map(x => (x.getAs[String]("_1"), x.getAs[String]("_2"), 1))
+            // use (Date, Name) as the key to group all counts for each pair
+            .groupBy("_1", "_2")
+            // compute the total count for each (Date, Name)
+            .agg(sum("_3"))
+            // sort in a descending order based on counts
+            .sort(desc("sum(_3)"))
         
-        
-        
+        // create groupings based on Date, displaying results in a count descending way
         val window = Window.partitionBy("_1").orderBy(desc("sum(_3)"))
-        df.select(col("*"), rank().over(window).alias("rank")).where(col("rank")<=10).show
-
         
-        
-                        
+        //Display results in a  Date, Name, Count, Rank format
+        df.select(col("*"), rank().over(window).alias("rank")).where(col("rank")<=10).show()
+                                   
+        // Stop Spark Session
         spark.stop
     }
 }
--- RDDs/src/main/scala/RDDs.scala
+++ RDDs/src/main/scala/RDDs.scala
@@ -2,7 +2,7 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 
-object implementationRDD {
+object GDeltRDD {
   def main(args: Array[String]) {
     // create Spark context with Spark configuration
     val conf = new SparkConf()
@@ -12,16 +12,44 @@ object implementationRDD {
     
     // parsing the csv files into input variable 
     val input = sc.textFile("../*.csv")
-    printf("Count %d\n", input.count)
     
-    //for each field extract the corresponding fields with a map(split), using a tab "\t" as a delimiter and considering blanks with "-1".
+    // for each field extract the corresponding fields with a map(split), using a tab "\t" as a delimiter and considering blanks with "-1".
     val fields = input.map(x => x.split("\t", -1))
-    val tuples = fields.filter(x=>x(23) != "").map(x=>(x(1).substring(0,8),x(23)))
+    val tuples = fields
+        // remove lines that do not contain the AllNames field
+        .filter(x=>x.length > 23)
+        // create tuples having as elements the Date and AllNames field for each text line
+        .map(x=>(x(1).substring(0,8),x(23)))
     
     // firstly discard the digits pointing to the reference of the name in the article; then separate, filter out blanks and FP and mapReduce. Finally sort and keep 10 highest
-    val topics = tuples.map(x => (x._1,x._2.replaceAll("[0-9]",""))).flatMapValues(x=> x.split("[;,]")).filter(x=>x._2!="" && x._2!="ParentCategory" && x._2!="CategoryType" && x._2!="Type ParentCategory").map(x=>((x._1,x._2),1)).reduceByKey(_+_).map(_.swap).sortByKey(false).map(_.swap).map(x=> (x._1._1,(x._1._2,x._2))).groupByKey().map(x=>Array(x._1,x._2.slice(0,10)))
+    val topics = tuples
+        // remove the offsets from the AllNames field
+        .map(x => (x._1,x._2.replaceAll("[0-9]","")))
+        // separate the Names and create a tuple for each containing the Date also
+        .flatMapValues(x=> x.split("[;,]"))
+        // remove wrong values
+        .filter(x=>x._2!="" && x._2!="ParentCategory" && x._2!="CategoryType" && x._2!="Type ParentCategory")
+        // emit 1 for each pair of Date and Name
+        .map(x=>((x._1,x._2),1))
+        // collect the total counts for each pair
+        .reduceByKey(_+_)
+        // change the tuple elements' order to use the counts as the key
+        .map(_.swap)
+        // sort using the counts in descending order
+        .sortByKey(false)
+        // change the tuple elements' order to its original form
+        .map(_.swap)
+        // change the key from the (Date, Name) pair to only the Date
+        .map(x=> (x._1._1,(x._1._2,x._2)))
+        // group using the Date
+        .groupByKey()
+        // get only the top 10 (Name, counts) pairs
+        .map(x=>Array(x._1,x._2.slice(0,10)))
+   
+    // print each tuple result as strings
     topics.foreach(a => println(a.mkString(" ")))
-    
+
+    // stop Spark's context
     sc.stop()
   }
 }
